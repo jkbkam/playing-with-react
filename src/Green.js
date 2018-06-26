767670aae6b5cb7762768de5c2e8a58733e68a54
Kuba <jkb.kam@gmail.com>
2018-06-26 14:18:31 +0200

    dodany component green

---
+++ src/Green.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './main.css';
+
+class Green extends Component {
+    render () {
+        return (
+            <div className="green">
+
+            </div>
+        )
+    }
+}
+
+export default Green;
